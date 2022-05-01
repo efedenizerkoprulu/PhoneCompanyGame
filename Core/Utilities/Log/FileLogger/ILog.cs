39ee9327ed29d9542552fa6863c045018d6dc60b
Efe Denizer Köprülü <43250848+EfeDenizerKoprulu@users.noreply.github.com>
2022-05-01 14:36:18 +0300

    Log

---
+++ Core/Utilities/Log/FileLogger/ILog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Log.FileLogger
+{
+    public interface ILog
+    {
+        void CreateLog(string fileName);
+        void WriteLog(string fileName, string text);
+        bool IslogExsist(string fileName);
+        string Path { get; set; }
+    }
+}
---
+++ Core/Utilities/Log/FileLogger/Log.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Log.FileLogger
+{
+    public class Log : ILog
+    {
+        public string Path { get; set; }
+
+        public void CreateLog(string fileName)
+        {
+            FileStream fs = File.Create(@"Log\"+ fileName + "AccountLog.txt");
+            fs.Close();
+        }
+
+        public bool IslogExsist(string fileName)
+        {
+            string path = @"Log\" + fileName + "AccountLog.txt";
+            bool isExsistFile = File.Exists(path);
+            if (isExsistFile)
+            {
+               Path = path;
+                return true;
+            }
+            else
+            {
+                CreateLog(fileName);
+                return false;
+            }
+        }
+
+        public void WriteLog(string fileName,string text)
+        {
+            if (IslogExsist(fileName))
+            {
+                StreamWriter sw = new StreamWriter(Path);
+                sw.WriteLine("Date and Time: "+DateTime.Now + "Error masage:" + text);
+                sw.Close();
+            }
+        }
+    }
+}
